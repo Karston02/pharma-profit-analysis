baa7cdf9126fb12aa4314db83013fb06b4c656af
Karston Kuciemba <kuciembakarston@gmail.com>
2024-04-19 11:50:58 -0500

    project continued...

---
+++ project/visualizations/presentation.py
@@ -0,0 +1,7 @@
+from visualizations import show_graph1
+from visualizations import show_graph2
+from visualizations import show_graph3
+from visualizations import show_graph4
+from visualizations import show_graph5
+
+show_graph1()
