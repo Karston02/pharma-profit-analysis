740ecb9aeb85ed9288437cfec02d96451ad72a55
Karston Kuciemba <kuciembakarston@gmail.com>
2024-04-22 20:22:19 -0500

    dashboard fixes + progress

--- project/visualizations/presentation.py
+++ project/visualizations/presentation.py
@@ -4,9 +4,18 @@ from dash import Dash, html
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
+
+    ################
+    # Introduction #
+    ################
     html.H1(children='Exploring the Intersection of COVID-19, Overdoses, and Pharmaceutical Market Dynamics ', className='header'),
     html.H3(children='Group 21: Karston Kuciemba, Ananya Kaalva, & Carolyn Pyun',
             className='subheader'),
+
+
+    ######################
+    # Purpose of Project #
+    ######################
     html.H3(children='Purpose & Hypothesis: ',
             className='section-header'),
     html.Ul(children=[
@@ -24,45 +33,67 @@ app.layout = html.Div(children=[
                 'Overdoses are seen as negligible to pharmaceutical companies’ bottom line (or stock price)'], className='list-item'),
     ]),
 
-
+    ###########
+    # Chart 1 #
+    ###########
     html.Iframe(srcDoc=open("vis1.html").read(), id='graph1',
                 className='chart'),
     html.Ul(children=[
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
+        html.Li(children=[
+                'There are 2 key notes in this - Moderna (MRNA) and Eli Lilly & Co (LLY)'], className='list-item'),
+        html.Ul(children=[
+            html.Li(children=[
+                    'Both vaccine producers, seemingly produced marginally significant returns when compared to their competitors in the industry even before they were announced to release a vaccine'], className='sublist-item'),
+            html.Li(children=[
+                    'Other vaccine producers, such as Pfizer (PFE) and JNJ, did not have near the result as the other producers, which leads me to question the significance that COVID had on these results'], className='sublist-item'),
+        ], className='list'),
+        html.Li(children=['sample item'], className='list-item'),
     ]),
 
+
+    ###########
+    # Chart 2 #
+    ###########
     html.Iframe(srcDoc=open("vis2.html").read(), id='graph2',
+                className='special-chart'),
+    html.Ul(children=[
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+    ]),
+
+    ###########
+    # Chart 3 #
+    ###########
+    html.Iframe(srcDoc=open("vis3.html").read(), id='graph3',
                 className='chart'),
     html.Ul(children=[
-        html.Li(children=['item 2'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
     ]),
 
-    html.Iframe(srcDoc=open("vis3.html").read(), id='graph3',
-                style={"height": "400px", "width": "800px"}),
-    html.Ul(children=[
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-    ]),
-
+    ###########
+    # Chart 4 #
+    ###########
     html.Iframe(srcDoc=open("vis4.html").read(), id='graph4',
-                style={"height": "400px", "width": "800px"}),
+                className='special-chart'),
     html.Ul(children=[
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
     ]),
 
+
+    ###########
+    # Chart 5 #
+    ###########
     html.Iframe(srcDoc=open("vis5.html").read(), id='graph5',
-                style={"height": "400px", "width": "800px"}),
+                className='final-chart'),
     html.Ul(children=[
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
-        html.Li(children=['item 23'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
+        html.Li(children=['sample item'], className='list-item'),
     ]),
 ], className='chart-wrapper')
 
